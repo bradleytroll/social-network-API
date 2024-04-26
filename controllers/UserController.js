1df3689734fd9b693c55c4f605aebdc3e720276f
Bradley Troll <bradleytroll@gmail.com>
2024-04-25 21:31:17 -0400

    Begins UserController.js - incomplete

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,19 @@
 const { User, Thoughts, Reaction } = require('../models');
 
 module.exports = {
-    getAllUsers
+    getAllUsers(req, res) {
+        User.find({})
+        .populate('thoughts')
+        .populate('friends')
+        .then(users => res.json(users))
+        .catch(err => res.status(500).json(err));
+    }, 
+//START HERE
 
 
 
 
- };
+
+
+
+}
