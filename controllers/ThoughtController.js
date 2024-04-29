696d3f37e0da94198f2ef3c33cecb9e019cdaf92
Bradley Troll <bradleytroll@gmail.com>
2024-04-29 09:03:29 -0400

    Adds thoguhtController.js

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -40,8 +40,52 @@ createThought(req, res) {
     .catch(err => res.status(500).json(err));
 }, 
 
-// START HERE
+// Update thought by ID
+updateThought(req, res) {
+    Thought.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+    .then(thought => {
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with that ID' });
+            }
+            res.json(thought);
+        })
+        .catch(err => res.status(500).json(err));
+},
 
+// Delete thought by ID
+deleteThought(req, res) {
+    Thought.findByIdAndDelete(req.params.id)
+    .then(thought => {
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with that ID' });
+            }
+            res.json({ message: 'Thought deleted successfully' });
+        })
+        .catch(err => res.status(500).json(err));
+},
 
+// Add a reaction to a thought
+addReaction(req, res) {
+    Thought.findByIdAndUpdate(req.params.thoughtId, { $push: { reactions: req.body } }, { new: true, runValidators: true })
+       .then(thought => {
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with that ID' });
+            }
+            res.json(thought);
+        })
+       .catch(err => res.status(500).json(err));
+},
 
+// Remove a reaction from a thought
+removeReaction(req, res) {
+    Thought.findByIdAndUpdate(req.params.thoughtId, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { new: true })
+       .then(thought => {
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with that ID' });
+            }
+            res.json(thought);
+        })
+       .catch(err => res.status(500).json(err));
 }
+
+};
